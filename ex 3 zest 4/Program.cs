a51fd452a39a51b2fad6a25cf15f93f08b9664f1
barbara.jamroga <barbara.jamroga@microsoft.wsei.edu.pl>
2019-05-16 09:08:57 +0200

    commit

--- ex 3 zest 4/Program.cs
+++ ex 3 zest 4/Program.cs
@@ -10,6 +10,31 @@ namespace ex_3_zest_4
     {
         static void Main(string[] args)
         {
+            Employee employee1 = new Employee();
+            employee1.Name = "John";
+            employee1.Surname = "Kowalski";
+            employee1.Age = 25;
+            employee1.HourlyRate = 30;
+
+            Console.WriteLine(employee1.Name + " " + employee1.Surname
+                + " earns " + employee1.CalculatePayment(140));
+
+            Console.ReadKey();
+        }
+        class Human
+        {
+            public string Name { get; set; }
+            public string Surname{ get; set; }
+            public int Age { get; set; }
+        }
+        class Employee : Human
+        {
+            public string Position { get; set; }
+            public float HourlyRate { get; set; }
+            public float CalculatePayment(int hoursNumber)
+            {
+                return hoursNumber * HourlyRate;
+            }
         }
     }
 }
